e3a2ad084134cae537f6a3a6b19c989f4f722548
bijay <bijay.mandal@primotech.com>
2023-11-17 18:08:02 +0530

    Login forgot password

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,7 @@ import 'package:real_twist/login.dart';
 import 'package:real_twist/spinwheelscreen.dart';
 
 import 'game2.dart';
+import 'menu.dart';
 import 'notification.dart';
 
 class HomeView extends StatefulWidget {
@@ -434,95 +435,7 @@ class CommonCard extends StatelessWidget {
           gradient: LinearGradient(
               colors: [Colors.pink.shade900, Colors.pinkAccent.shade100]),
         ),
-        child: child ?? SizedBox(),
-      ),
-    );
-  }
-}
-
-class DrawerView extends StatelessWidget {
-  const DrawerView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Colors.black,
-      child: ListView(
-        padding: const EdgeInsets.all(0),
-        children: [
-          DrawerHeader(
-            decoration:
-                const BoxDecoration(color: Colors.transparent), //BoxDecoration
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                gradient: LinearGradient(
-                    colors: [Colors.pink.shade900, Colors.pinkAccent.shade100]),
-              ),
-              child: const UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: Colors.transparent),
-                accountName: Text(
-                  "Abhishek Mishra",
-                  style: TextStyle(fontSize: 18),
-                ),
-                accountEmail: Text("abhishekm977@gmail.com"),
-                currentAccountPictureSize: Size.square(50),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Color.fromARGB(255, 165, 255, 137),
-                  child: Text(
-                    "A",
-                    style: TextStyle(fontSize: 30.0, color: Colors.blue),
-                  ), //Text
-                ), //circleAvatar
-              ),
-            ), //UserAccountDrawerHeader
-          ), //DrawerHeader
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text(' My Profile '),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.book),
-            title: const Text(' My Course '),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.workspace_premium),
-            title: const Text(' Go Premium '),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.video_label),
-            title: const Text(' Saved Videos '),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.edit),
-            title: const Text(' Edit Profile '),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('LogOut'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginView()),
-              );
-            },
-          ),
-        ],
+        child: child ?? const SizedBox(),
       ),
     );
   }
---
+++ lib/payment_view.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+// import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+
+class PaymentView extends StatefulWidget {
+  const PaymentView({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentView> createState() => _PaymentViewState();
+}
+
+class _PaymentViewState extends State<PaymentView> {
+  Map<String, dynamic>? paymentIntent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+          child: GestureDetector(
+            onTap: (){
+              makePayment();
+            },
+              child: const Text(
+        "Payment",
+        style: TextStyle(color: Colors.white),
+      ))),
+    );
+  }
+  Future<void> makePayment() async {
+    try {
+      paymentIntent = await createPaymentIntent('10000', 'INR');
+
+      // var gpay = PaymentSheetGooglePay(
+      //     merchantCountryCode: "GB", currencyCode: "GBP", testEnv: true);
+
+      //STEP 2: Initialize Payment Sheet
+      // await Stripe.instance
+      //     .initPaymentSheet(
+      //     paymentSheetParameters: SetupPaymentSheetParameters(
+      //         paymentIntentClientSecret: paymentIntent![
+      //         'client_secret'], //Gotten from payment intent
+      //         style: ThemeMode.light,
+      //         merchantDisplayName: 'Abhi',
+      //         googlePay: gpay))
+      //     .then((value) {});
+
+      //STEP 3: Display Payment sheet
+      displayPaymentSheet();
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  displayPaymentSheet() async {
+    try {
+      // await Stripe.instance.presentPaymentSheet().then((value) {
+      //   print("Payment Successfully");
+      // });
+    } catch (e) {
+      print('$e');
+    }
+  }
+
+  createPaymentIntent(String amount, String currency) async {
+    try {
+      Map<String, dynamic> body = {
+        'amount': amount,
+        'currency': currency,
+      };
+
+      var response = await http.post(
+        Uri.parse('https://api.stripe.com/v1/payment_intents'),
+        headers: {
+          'Authorization':
+          'Bearer sk_test_51MWx8OAVMyklfe3C3gP4wKOhTsRdF6r1PYhhg1PqupXDITMrV3asj5Mmf0G5F9moPL6zNfG3juK8KHgV9XNzFPlq00wmjWwZYA',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body,
+      );
+      return json.decode(response.body);
+    } catch (err) {
+      throw Exception(err.toString());
+    }
+  }
+}
+
