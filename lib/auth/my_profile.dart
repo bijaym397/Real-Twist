f734e8a30328aa1f91461a4864c12ed68de9df17
bijay <bijay.mandal@primotech.com>
2023-11-21 17:23:24 +0530

    my profile

--- lib/auth/my_profile.dart
+++ lib/auth/my_profile.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 final String name = "Bijay";
+final String img = "assets/user.png";
 
 class MyProfile extends StatelessWidget {
   const MyProfile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    const String name = "Bijay";
+    const String img = "assets/user.png";
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -21,19 +24,21 @@ class MyProfile extends StatelessWidget {
           const SizedBox(height: 65),
           Container(
             decoration: const BoxDecoration(
-                shape: BoxShape.circle, color: Colors.white70),
+                shape: BoxShape.circle, color: Colors.white70,
+            ),
             child: Container(
-              height: 200,
-              width: 200,
+              height: 150,
+              width: 150,
               margin: EdgeInsets.all(8),
               decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(img)),
                   color: Colors.pink.shade600, shape: BoxShape.circle),
-              child: Center(
+              child: img== "" ? Center(
                   child: Text(
-                name,
+                name[0].toUpperCase(),
                 style:
                     const TextStyle(fontWeight: FontWeight.bold, fontSize: 52),
-              )),
+              )): SizedBox(),
             ),
           ),
           const SizedBox(height: 32),
