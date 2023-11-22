949d06278f941e1d334785faf10af8c4e0dd7e40
Munish <Munish.primotech@gmail.com>
2023-11-22 18:29:10 +0530

    base url

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,7 +1,110 @@
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:real_twist/otp_verification_screen.dart'; // Import the OTP verification screen
+// import 'package:http/http.dart' as http;
+//
+// class LoginView extends StatefulWidget {
+//   const LoginView({Key? key}) : super(key: key);
+//
+//   @override
+//   State<LoginView> createState() => _LoginViewState();
+// }
+//
+// class _LoginViewState extends State<LoginView> {
+//   final TextEditingController phoneNumberController = TextEditingController();
+//   final String countryCode = '+91'; // Static country code
+//
+//   Future<void> _hitLoginApi() async {
+//     // final apiUrl = '{{host}}login';
+//     // final response = await http.post(
+//     //   Uri.parse(apiUrl),
+//     //   headers: {'Content-Type': 'application/json'},
+//     //   body: jsonEncode({
+//     //     'phoneNumber': '$countryCode${phoneNumberController.text}',
+//     //     'countryCode': countryCode,
+//     //   }),
+//     // );
+//     //
+//     // if (response.statusCode == 200) {
+//       // API call successful
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(
+//           content: const Text("OTP sent to your phone."),
+//         ),
+//       );
+//
+//       // Navigate to OTP verification screen
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(
+//           builder: (context) => OtpVerificationScreen(
+//             phoneNumber: '$countryCode${phoneNumberController.text}',
+//           ),
+//         ),
+//       );
+//     // } else {
+//     //   // API call failed
+//     //   ScaffoldMessenger.of(context).showSnackBar(
+//     //     SnackBar(
+//     //       content: const Text("Failed to send OTP. Please try again."),
+//     //     ),
+//     //   );
+//     //}
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: ListView(
+//           padding: const EdgeInsets.all(16),
+//           children: [
+//             const SizedBox(height: 120),
+//             const Center(
+//                 child: Text(
+//               "Welcome to",
+//               style: TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
+//             )),
+//             const SizedBox(height: 8),
+//             const Center(
+//                 child: Text(
+//               "Real Twist",
+//               style: TextStyle(fontWeight: FontWeight.w800, fontSize: 30),
+//             )),
+//             const SizedBox(height: 60),
+//             TextFormField(
+//               controller: phoneNumberController,
+//               keyboardType: TextInputType.phone,
+//               decoration: const InputDecoration(
+//                 prefixIcon: Icon(Icons.phone),
+//                 contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+//                 border: OutlineInputBorder(),
+//                 hintText: 'Enter Your Phone Number',
+//                 labelText: "Enter Your Phone Number",
+//               ),
+//             ),
+//             const SizedBox(height: 24),
+//
+//             SizedBox(
+//               height: 45,
+//               child: ElevatedButton(
+//                 child: const Text("Login", style: TextStyle(fontSize: 18)),
+//                 onPressed: _hitLoginApi, // Call the API function on button press
+//               ),
+//             ),
+//             const SizedBox(height: 60),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+///
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:real_twist/admin/dashboard.dart';
 import 'package:real_twist/constants/api.dart';
 import 'package:real_twist/constants/strings.dart';
 import 'package:real_twist/home.dart';
@@ -60,8 +163,7 @@ class _LoginViewState extends State<LoginView> {
                   controller: phoneController,
                   maxLength: 10,
                   validator: (value) {
-                    if (value == null || value.trim().isEmpty)
-                      return "*Required";
+                    if (value == null || value.trim().isEmpty) return "*Required";
                     if (value.length < 10) {
                       return "Length should be 10";
                     }
@@ -70,16 +172,15 @@ class _LoginViewState extends State<LoginView> {
                   },
                   // validator: FromValidator.phoneValidator,
                   textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.phone,
                   inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                   decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.phone),
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Your Phone Number',
-                    labelText: "Enter Your Phone Number",
-                    counterText: "",
+                      prefixIcon: Icon(Icons.phone),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Your Phone Number',
+                      labelText: "Enter Your Phone Number",
+                      counterText: "",
                   ),
                 ),
                 const SizedBox(height: 24),
@@ -88,8 +189,7 @@ class _LoginViewState extends State<LoginView> {
                   autovalidateMode: AutovalidateMode.onUserInteraction,
                   controller: passwordController,
                   validator: (value) {
-                    if (value == null || value.trim().isEmpty)
-                      return "*Required";
+                    if (value == null || value.trim().isEmpty) return "*Required";
                     if (value.length < 8) {
                       return "Password must contain at least 8 characters.";
                     }
@@ -102,23 +202,28 @@ class _LoginViewState extends State<LoginView> {
                   textInputAction: TextInputAction.done,
                   obscureText: hidePassword,
                   decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-                    prefixIcon: const Icon(Icons.key),
-                    border: const OutlineInputBorder(),
-                    hintText: 'Enter You Password',
-                    labelText: "Enter You Password",
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        hidePassword = !hidePassword;
-                        setState(() {});
-                      },
-                      child: Icon(
-                        hidePassword
-                            ? Icons.visibility_off_rounded
-                            : Icons.visibility_rounded,
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                      prefixIcon: const Icon(Icons.key),
+                      border: const OutlineInputBorder(),
+                      hintText: 'Enter You Password',
+                      labelText: "Enter You Password",
+                      suffixIcon: InkWell(
+                        onTap: (){
+                          hidePassword = !hidePassword;
+                          setState(() {
+
+                          });
+                        },
+                          child: Icon(
+                          hidePassword
+                          ? Icons.visibility_off_rounded
+                              : Icons.visibility_rounded,
+                          ),
+                        /*onTap: () {
+
+                        }*/
                       ),
-                    ),
                   ),
                 ),
                 const SizedBox(height: 12),
@@ -130,8 +235,8 @@ class _LoginViewState extends State<LoginView> {
                       },
                       child: const Text(
                         "Forgot Password",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 16),
+                        style:
+                            TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
                       ),
                     )),
                 const SizedBox(height: 32),
@@ -139,8 +244,7 @@ class _LoginViewState extends State<LoginView> {
                   height: 45,
                   child: CommonCard(
                     onTap: () {
-                      // _showReferDialog(context);
-                      // Navigator.of(context).push(MaterialPageRoute(builder: (context) => OtpVerificationScreen()));
+                      // Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeView()));
                       if (loginFormKey.currentState!.validate()) {
                         _hitLoginApi(
                           phone: phoneController.text.trim(),
@@ -151,8 +255,8 @@ class _LoginViewState extends State<LoginView> {
                     child: const Center(
                       child: Text(
                         'Login',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w600),
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
                       ),
                     ),
                   ),
@@ -161,9 +265,7 @@ class _LoginViewState extends State<LoginView> {
                 GestureDetector(
                   onTap: () {
                     Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute(
-                            builder: (context) => const SignupView()),
-                        (Route<dynamic> route) => false);
+                      MaterialPageRoute(builder: (context) => const SignupView()),(Route<dynamic> route) => false);
                   },
                   child: RichText(
                     textAlign: TextAlign.center,
@@ -195,7 +297,8 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 
-  Future<void> _hitLoginApi({String? phone, String? password}) async {
+  Future<void> _hitLoginApi(
+      {String? phone, String? password}) async {
     try {
       customLoader!.show(context);
       const apiUrl = "${Api.baseUrl}${Api.login}";
@@ -208,7 +311,7 @@ class _LoginViewState extends State<LoginView> {
           'password': password.toString(),
         }),
       );
-      final loginData = LoginDataResponse.fromJson(json.decode(response.body));
+      final loginData = LoginData.fromJson(json.decode(response.body));
       if (response.statusCode == 200) {
         customLoader!.hide();
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -220,15 +323,11 @@ class _LoginViewState extends State<LoginView> {
             AppStrings.spUserId, loginData.data!.userId.toString());
         sharedPreferences.setString(
             AppStrings.spAuthToken, loginData.data!.authToken.toString());
-        sharedPreferences.setString(
-            AppStrings.spUserType, loginData.data!.user!.userType.toString());
-        // ignore: use_build_context_synchronously
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-              builder: (context) => Api.userType != loginData.data!.user!.userType.toString()
-                  ? const DashboardView()
-                  : HomeView()),
+            builder: (context) => const HomeView(),
+          ),
         );
       } else {
         // API call failed
@@ -263,10 +362,8 @@ class _LoginViewState extends State<LoginView> {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
           content: Text("${forgotData.message}"),
         ));
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-            builder: (context) => OtpVerificationScreen(
-                token: forgotData.data!.token.toString(),
-                phoneNumber: phone.toString())));
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => OtpVerificationScreen(
+        token: forgotData.data!.token.toString(),phoneNumber: phone.toString())));
       } else {
         // API call failed
         customLoader!.hide();
@@ -280,110 +377,6 @@ class _LoginViewState extends State<LoginView> {
       customLoader!.hide();
       return debugPrint(e.toString());
     }
-  }
-
-  Future<void> _showReferDialog(BuildContext context) async {
-    TextEditingController referController = TextEditingController();
-    FocusNode referNode = FocusNode();
-
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      barrierColor: Colors.black87,
-      builder: (context) {
-        final GlobalKey<FormState> referFormKey = GlobalKey<FormState>();
-        return AlertDialog(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-          ),
-          content: Container(
-            height: 200,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-            ),
-            child: Form(
-              key: referFormKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  const Text('Referral Code?', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  const Text('Please Enter the phone number of your friend who refer\'s us.', style: TextStyle(fontSize: 18),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(width: 10),
-                  TextFormField(
-                    controller: referController,
-                    focusNode: referNode,
-                    maxLength: 10,
-                    keyboardType: TextInputType.phone,
-                    textInputAction: TextInputAction.done,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return "*Required";
-                      }
-                      if (value.length < 10) {
-                        return "Length should be 10";
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      contentPadding:
-                      EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-                      prefixIcon: Icon(Icons.key),
-                      border: OutlineInputBorder(),
-                      hintText: '998xx xxxxx',
-                      labelText: "Enter Phone Number",
-                      counterText: "",
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  SizedBox(
-                    height: 40,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: CommonCard(
-                            onTap: () => Navigator.pop(context),
-                            child: const Center(
-                              child: Text(
-                                'Skip',
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: CommonCard(
-                            onTap: () {
-                              if (referFormKey.currentState!.validate()) {
-                                // _hitForgotApi(
-                                //     phone: textFieldController.text.trim());
-                              }
-                            },
-                            child: const Center(
-                              child: Text(
-                                'Submit',
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
   }
 
   Future<void> _showTextFieldPopup(BuildContext context) async {
@@ -411,7 +404,7 @@ class _LoginViewState extends State<LoginView> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  const Text('Forgot Password', style: TextStyle(fontSize: 20)),
+                  const Text('Forgot Password',style: TextStyle(fontSize: 20)),
                   TextFormField(
                     controller: textFieldController,
                     focusNode: forgotFocusNode,
@@ -430,7 +423,7 @@ class _LoginViewState extends State<LoginView> {
                     },
                     decoration: const InputDecoration(
                       contentPadding:
-                          EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 6),
                       prefixIcon: Icon(Icons.key),
                       border: OutlineInputBorder(),
                       hintText: 'xxxxx xxxxx',
@@ -458,11 +451,9 @@ class _LoginViewState extends State<LoginView> {
                         Expanded(
                           child: CommonCard(
                             onTap: () {
-
-                              // if (forgotFormKey.currentState!.validate()) {
-                              //   _hitForgotApi(
-                              //       phone: textFieldController.text.trim());
-                              // }
+                              if(forgotFormKey.currentState!.validate()){
+                                _hitForgotApi(phone: textFieldController.text.trim());
+                              }
                             },
                             child: const Center(
                               child: Text(
@@ -484,4 +475,5 @@ class _LoginViewState extends State<LoginView> {
       },
     );
   }
+
 }
