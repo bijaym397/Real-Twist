5702ac62eb181733a2082f3b463ba13e106fe27c
bijay <bijay.mandal@primotech.com>
2023-11-23 17:08:17 +0530

    my profile

--- lib/admin/set_coins_price.dart
+++ lib/admin/set_coins_price.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../home.dart';
-
-class SetCoinPrice extends StatefulWidget {
-  const SetCoinPrice({Key? key}) : super(key: key);
-
-  @override
-  State<SetCoinPrice> createState() => _SetCoinPriceState();
-}
-
-class _SetCoinPriceState extends State<SetCoinPrice> {
-  final GlobalKey<FormState> setCoinFormKey = GlobalKey<FormState>();
-  TextEditingController setCoinCont = TextEditingController();
-  FocusNode phoneNode = FocusNode();
+class CoinPrice extends StatelessWidget {
+  const CoinPrice({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 8,
-        centerTitle: true,
-        backgroundColor: Colors.pink.shade800,
-        title: const Text("My Profile"),
-        automaticallyImplyLeading: true,
-      ),
-      backgroundColor: Colors.black87,
-
-    );
+    return const Placeholder();
   }
 }
